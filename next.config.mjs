cf9519b417588b2dba946a80aa3283ce38d1b5ab
semended <gsemion291007@gmail.com>
2026-02-26 14:46:58 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,3 +6,12 @@ const nextConfig = {
 };
 
 export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
